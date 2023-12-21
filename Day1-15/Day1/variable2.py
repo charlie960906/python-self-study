8047ada49146cd5dac3a39d2bfb10109c1ceb2f9
charlie960906 <charlie960906@gmail.com>
2023-12-21 22:51:02 +0800

    Day1

---
+++ Day1-15/Day1/variable2.py
@@ -0,0 +1,20 @@
+"""
+使用type()檢查變數類型
+"""
+
+a=100
+b=12.345
+c=1 + 5j
+d='hello,world'
+e=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+#<class 'int'>
+#<class 'float'>
+#<class 'complex'>
+#<class 'str'>
+#<class 'bool'>
