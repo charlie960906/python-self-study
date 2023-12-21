67d9b5d94d9af5401f6a800b6d559e481b850793
charlie960906 <charlie960906@gmail.com>
2023-12-21 23:00:21 +0800

    Day1

---
+++ Day1-15/Day1/variable3.py
@@ -0,0 +1,14 @@
+"""
+使用input()函數輸入(字串)
+使用int()函數將輸入轉換成整數
+"""
+a=int(input('a='))
+b=int(input('b='))
+print('%d + %d = %d' % (a,b,a+b))
+print('%d - %d = %d' % (a,b,a-b))
+print('%d * %d = %d' % (a,b,a*b))
+print('%d / %d = %d' % (a,b,a/b))
+print('%d // %d = %d' % (a,b,a//b))
+print('%d %% %d = %d' % (a,b,a % b))
+print('%d ** %d = %d' % (a,b,a**b))
+
