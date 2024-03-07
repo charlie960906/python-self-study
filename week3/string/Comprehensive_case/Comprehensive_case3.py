cceb7a7c550fb9e17d25e3e6d8911a4b072fe006
charlie960906 <charlie960906@gmail.com>
2024-03-07 11:15:16 +0800

    Comprehensive_case3

---
+++ week3/string/Comprehensive_case/Comprehensive_case3.py
@@ -0,0 +1,46 @@
+import os
+
+
+def print_board(board):
+    print(board['TL'] + '|' + board['TM'] + '|' + board['TR'])
+    print('-+-+-')
+    print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
+    print('-+-+-')
+    print(board['BL'] + '|' + board['BM'] + '|' + board['BR'])
+
+
+def main():
+    init_board = {
+        'TL': ' ', 'TM': ' ', 'TR': ' ',
+        'ML': ' ', 'MM': ' ', 'MR': ' ',
+        'BL': ' ', 'BM': ' ', 'BR': ' '
+    }
+    begin = True
+    while begin:
+        curr_board = init_board.copy()
+        begin = False
+        turn = 'x'
+        counter = 0
+        os.system('clear')
+        print_board(curr_board)
+        while counter < 9:
+            move = input('輪到%s走棋,請輸入位置: ' % turn)
+            if curr_board[move] == ' ':
+                counter += 1
+                curr_board[move] = turn
+                if turn == 'x':
+                    turn = 'o'
+                else:
+                    turn = 'x'
+            os.system('clear')
+            print_board(curr_board)
+        choice = input('再玩一局?(yes or no)')
+        begin = choice == 'yes'
+
+
+if __name__ == '__main__':
+    main()
+
+'''
+之後添加判斷遊戲是誰勝利
+'''
