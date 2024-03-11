461aa9f89d73e3c661d7706279cdb45983d445d1
charlie960906 <charlie960906@gmail.com>
2024-03-11 14:37:24 +0800

    class2

---
+++ week3/object/class/class2.py
@@ -0,0 +1,18 @@
+class Test:
+    
+    def __init__(self,foo):
+        self.__foo=foo
+    
+    def __bar(self):
+        print(self.foo)
+        print('__bar')
+    
+
+def main():
+        test = Test('hello')
+        test.__bar()
+        print(test.__foo)
+
+
+if __name__ == "__main__":
+        main
